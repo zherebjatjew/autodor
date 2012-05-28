e3b90b2069f46f1fe7b139d400704e2d7f62e06b
Dmitry Zherebjatjew <dzherebjatjew@thumbtack.net>
2012-05-28 20:17:11 +0700

    Chapter 5

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Autodor::Application.routes.draw do
   get "pages/home"
 
   get "pages/contact"
+  
+  get "pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,20 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
+  
+  before(:each) do
+    @base_title = 'Ruby on Rails Tutorial Sample App | '
+  end
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
+    end
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content => @base_title + "Home")
     end
   end
 
@@ -13,6 +22,18 @@ describe PagesController do
     it "returns http success" do
       get 'contact'
       response.should be_success
+      response.should have_selector("title", :content => @base_title + "Contact")
+    end
+  end
+
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+    it "returns http success" do
+      get 'about'
+      response.should have_selector("title", :content => @base_title + "About")
     end
   end
 
