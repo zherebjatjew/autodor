842c530418f852394585d3f649e345fe30467989
Dmitry Zherebjatjew <dzherebjatjew@thumbtack.net>
2012-06-01 17:02:53 +0700

    +Gemfile.lock

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
+
+  validates :name,  :presence => true,
+                    :length   => { :maximum => 50 }
+  validates :email, :presence => true,
+                    :format => { :with => /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    :uniqueness => { :case_sensitive => false }
 end
