931b2a31c79ee1a7d8c10113410f1ba9fd45ffef
Dmitrij <zherebjatjew@gmail.com>
2012-06-24 21:46:53 +0700

    utf-8 markers for heroku

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 class Order < ActiveRecord::Base
   attr_accessible :info
   belongs_to :user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 require 'digest'
 
 class User < ActiveRecord::Base
