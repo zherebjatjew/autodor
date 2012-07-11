c18ceeadcd586b24d284f899b2307716759bdd0d
zherebjatjew <zherebjatjew@gmail.com>
2012-07-11 23:56:29 +0700

    Embedded cargo forms

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,20 +1,24 @@
 # encoding: UTF-8
 
 class Order < ActiveRecord::Base
-  attr_accessible :info, :forwarder_id, :committed_at, :signed, :paid, :completed, :client_id, :sender_id, :receiver_id
+  attr_accessible :info, :forwarder_id, :committed_at,
+                  :signed, :paid, :completed, :client_id,
+                  :sender_id, :receiver_id, :cargos_attributes
 
   belongs_to :user
   has_one :forwarder, :class_name => 'User'
   has_one :client
   has_one :sender, :class_name => 'Client'
   has_one :receiver, :class_name => 'Client'
-  has_many :cargo
+  has_many :cargos
   
   validates :user_id, :presence => true
   validates :forwarder_id, :presence => true
   validates :client_id, :presence => true
   validates :sender_id, :presence => true
   validates :receiver_id, :presence => true
+
+  accepts_nested_attributes_for :cargos
 
   def forwarder
   	User.find forwarder_id
@@ -34,10 +38,6 @@ class Order < ActiveRecord::Base
 
   def receiver
     Client.find receiver_id
-  end
-
-  def cargos
-    Cargo.find(:all, :conditions => "order_id=#{id}")
   end
 
 end
