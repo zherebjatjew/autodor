d2325e3cc56397b8921c974208e843831500dc98
Dmitrij Zherebjatjew <dzherebjatjew@gmail.com>
2012-09-03 16:15:46 +0700

    New cargo records can be added!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -34,5 +34,23 @@ module ApplicationHelper
   def date_to_s date
     date.strftime "%d.%m.%Y" if !date.nil?
   end
-  
+
+  def add_object_link(name, form, object, partial, where)
+    options = {:parent => true}#.merge(options)
+    html = render(:partial => partial, :locals => { :form => form, :object => object } )
+    link_to_function name, %{
+      var new_object_id = new Date().getTime() ;
+      var html = jQuery(#{js html}.replace(/index_to_replace_with_js/g, new_object_id)).hide();
+      html.appendTo(jQuery("#{where}")).slideDown('slow');
+    }
+  end
+
+  def js(data)
+    if data.respond_to? :to_json
+      data.to_json
+    else
+      data.inspect.to_json
+    end
+  end
+
 end
