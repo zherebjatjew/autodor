8ea1b5447b9b3f9048aa291bbb3618352d0896b1
Dmitrij Zherebjatjew <dzherebjatjew@gmail.com>
2012-09-07 13:01:11 +0700

    Typo

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,6 +1,6 @@
 # encoding : UTF-8
 
-cclass Driver < ActiveRecord::Base
+class Driver < ActiveRecord::Base
   attr_accessible :name, :born_at, :identity, :address, :note, :phone
   has_many :orders
 end
