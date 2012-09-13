d5a1d3a4a43405dc529bb65cf1806c5ec00119e0
Dmitrij Zherebjatjew <zherebjatjew@gmail.com>
2012-09-13 12:53:04 +0700

    Syntax error

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -3,7 +3,7 @@
 class CarOwner < ActiveRecord::Base
   attr_accessible :car_id, :driver_id
 
-  has_many cars
-  has_many drivers
+  has_many :cars
+  has_many :drivers
   belongs_to :author, :class_name => 'User'
 end
