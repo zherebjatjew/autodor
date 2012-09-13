efffc3c5d61feb8a43f7daf5416738bd6e0ef40c
Dmitrij Zherebjatjew <zherebjatjew@gmail.com>
2012-09-13 18:52:24 +0700

    +Shipper.account

--- app/models/shipper.rb
+++ app/models/shipper.rb
@@ -1,7 +1,7 @@
 # encoding : UTF-8
 
 class Shipper < ActiveRecord::Base
-  attr_accessible :address1, :address2, :ati, :bic, :contact, :corr_account, :director, :director_short, :email, :registration, :inn, :kpp, :name, :ogrn, :phones
+  attr_accessible :account, :address1, :address2, :ati, :bic, :contact, :corr_account, :director, :director_short, :email, :registration, :inn, :kpp, :name, :ogrn, :phones
 
   has_many :drivers
   belongs_to :author, :class_name => 'User'
