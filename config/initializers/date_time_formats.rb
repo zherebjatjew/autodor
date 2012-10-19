7a668e72b6a8aed6469dd393006d806e223c646f
Dmitrij Zherebjatjew <zherebjatjew@gmail.com>
2012-10-19 10:33:02 +0700

    Database datetime format

--- config/initializers/date_time_formats.rb
+++ config/initializers/date_time_formats.rb
@@ -1,4 +1,4 @@
 Date::DATE_FORMATS[:default] = '%d.%m.%Y'
 Time::DATE_FORMATS[:default] = '%d.%m.%Y %H:%M:%S'
-Time::DATE_FORMATS[:db] = '%d.%m.%Y %H:%M:%S'
+Time::DATE_FORMATS[:db] = '%m.%d.%Y %H:%M:%S'
 
