e1eadbe8953c99e8efc854ee4a68feeaab89bdf5
Dmitrij Zherebjarjew <zherebjatjew@gmail.com>
2012-10-30 21:23:47 +0700

    Restyle

--- app/assets/javascripts/ajax.js
+++ app/assets/javascripts/ajax.js
@@ -1,16 +1,22 @@
 function editDialog(e, xhr, status) {
   var url = $j(this).attr('href');
   var header = $j(this).attr('alias');
+  var main = $j('#main');
   var dialog_form = $j('<div id="dialog-form">Загрузка формы...</div>').dialog({
+    title: header,
     autoOpen: false,
-    width: 520,
+    width: main.width()-20,
+    position: { my: "right top", at: "right top", of: main },
+//    position: [main.offset().left+20, -30],
     modal: true,
+    resizable: false,
+    draggable: false,
     zIndex : 500,
-    title: $j(this).attr('alias'),
-    closeText: 'Закрыть',
+    minHeight: main.height(),
+    closeText: 'x',
     open: function() {
-      $j('#dialog-form').append($j(xhr.responseText).find('form'));
-      $j('#dialog-form').html(xhr.responseText);
+      var form = $j('#dialog-form');
+      form.html(xhr.responseText);
     },
     close: function() {
       $j('#dialog-form').remove();
